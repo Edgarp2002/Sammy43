d298edab61f6613681fd72a47ebd1f7936dfb135
Edgarp2002 <113042036+Edgarp2002@users.noreply.github.com>
2022-09-16 12:46:44 -0500

    Create suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,11 @@
+utilizando el espacio de  nombres  estándar ;
+int  principal ()
+{
+	flotante  x,y,z ;
+	cout<< " Ingreso dos numeros separados de espacio xy:  " ;
+	cin>> x>>y ;
+	z=x+y ;
+	cout<< " El resultado es: " ;
+	cout<< z<<endl ;
+	devolver  0 ;
+}
