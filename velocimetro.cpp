1505f9e118268ca829503172be768e603258f3f3
Edgarp2002 <113042036+Edgarp2002@users.noreply.github.com>
2022-09-16 12:53:33 -0500

    Create velocimetro.cpp

---
+++ velocimetro.cpp
@@ -0,0 +1,8 @@
+int main ()
+{
+	float v,d,t;
+	cout<<"Ingrese la distancia(en km) d=: ";cin>>d;
+	cout<<"Ingrese el tiempo(en horas) t=: ";cin>>t;
+	v=d/t;
+	cout<<"La velocidad calculada es de v= "<<v<<endl<<endl;
+	return 0;
