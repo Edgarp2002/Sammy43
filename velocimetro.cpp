462a139e489369778d1d31dfa85062e698fb1b21
Your Name <you@example.com>
2022-09-25 01:27:36 +0000

    version1

--- velocimetro.cpp
+++ velocimetro.cpp
@@ -1,8 +1,16 @@
-int main ()
+#include<iostream>
+using namespace std;
+
+int main()
 {
 	float v,d,t;
-	cout<<"Ingrese la distancia(en km) d=: ";cin>>d;
-	cout<<"Ingrese el tiempo(en horas) t=: ";cin>>t;
+	cout<<"Ingrese la distancia(en km) d= "; cin>>d;
+	cout<<"Ingrese el tiempo(en horas)  t= "; cin>>t;
 	v=d/t;
-	cout<<"La velocidad calculada es de v= "<<v<<endl<<endl;
+	cout<<"La velocidad calculada es de v= "<<t<<endl<<endl;
 	return 0;
+
+
+
+}
+
